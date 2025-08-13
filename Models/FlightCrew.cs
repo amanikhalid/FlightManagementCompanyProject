c6249b400972511f5e692841765989f2356bf7dd
Amani Khalid <amanikhalidd@gmail.com>
2025-08-13 11:01:51 +0400

    Navigation properties/ one-to-many relationship with Flight

--- Models/FlightCrew.cs
+++ Models/FlightCrew.cs
@@ -9,5 +9,9 @@ namespace FlightManagementCompanyProject.Models
     public class FlightCrew
     {
         public string RoleOnFlight { get; set; } // Role of the crew member on the flight (e.g., pilot, co-pilot, flight attendant)
+
+        // Navigation properties
+        // one-to-many relationship with Flight
+        public int FlightId { get; set; } // Foreign key to the Flight
     }
 }
