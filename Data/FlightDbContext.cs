56195decf855ee02e1955f9653aeaa197142837c
Amani Khalid <amanikhalidd@gmail.com>
2025-08-13 12:34:59 +0400

    Foreign key in Route table

--- Data/FlightDbContext.cs
+++ Data/FlightDbContext.cs
@@ -135,6 +135,7 @@ namespace FlightManagementCompanyProject.Data
             modelBuilder.Entity<Route>()
                 .HasOne(r => r.DestinationAirport) // Each route has one destination airport
                 .WithMany(a => a.DestinationRoutes) // An airport can have many destination routes
+                .HasForeignKey(r => r.DestinationAirportId) // Foreign key in Route table
 
 
 
