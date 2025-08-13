60c927c22842baa056980fae2cc447d88aeaa9d7
Amani Khalid <amanikhalidd@gmail.com>
2025-08-13 12:47:19 +0400

    unique constraint for DepartureUtc

--- Data/FlightDbContext.cs
+++ Data/FlightDbContext.cs
@@ -166,6 +166,11 @@ namespace FlightManagementCompanyProject.Data
                 .IsRequired()
                 .HasColumnType("datetime"); // Specify the column type as datetime
 
+            // unique constraint for DepartureUtc
+            modelBuilder.Entity<Flight>()
+                .HasIndex(f => f.DepartureUtc)
+                
+
 
 
 
