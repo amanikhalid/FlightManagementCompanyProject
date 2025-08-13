fcb85104a8434d51407f184f5889ff50aa2b3e85
Amani Khalid <amanikhalidd@gmail.com>
2025-08-13 11:29:55 +0400

    DbSet for Aircraft entities

--- Data/FlightDbContext.cs
+++ Data/FlightDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FlightManagementCompanyProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace FlightManagementCompanyProject.Data
 {
-    class FlightDbContext
+    public class FlightDbContext : DbContext // DbContext for the Flight Management Company Project
     {
+        public DbSet<Aircraft> Aircrafts { get; set; } // DbSet for Aircraft entities
     }
 }
