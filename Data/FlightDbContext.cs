51af6eae1299353a2faf780bf7194628940c3712
Amani Khalid <amanikhalidd@gmail.com>
2025-08-14 08:23:55 +0400

    Specify the column type as nvarchar(50)

--- Data/FlightDbContext.cs
+++ Data/FlightDbContext.cs
@@ -467,6 +467,7 @@ namespace FlightManagementCompanyProject.Data
                 .Property(cm => cm.FullName) // Full name is required
                 .IsRequired()
                 .HasMaxLength(50) // Maximum length of 50 characters
+                .HasColumnType("nvarchar(50)"); // Specify the column type as nvarchar(50)
 
 
 
