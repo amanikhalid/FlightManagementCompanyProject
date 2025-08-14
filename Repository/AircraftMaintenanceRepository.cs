e6819698ce49b05538ed0d38bd43acea599f03e7
Amani Khalid <amanikhalidd@gmail.com>
2025-08-14 10:59:20 +0400

    Constructor to inject the DbContext

--- Repository/AircraftMaintenanceRepository.cs
+++ Repository/AircraftMaintenanceRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightManagementCompanyProject.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace FlightManagementCompanyProject.Repository
 {
-    public class AircraftMaintenanceRepository
+    public class AircraftMaintenanceRepository : IAircraftMaintenanceRepository 
+
     {
+        private readonly FlightDbContext _context; // Constructor to inject the DbContext
     }
 }
