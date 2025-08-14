e005681b2b2180362d09875f41ad18aaa79bec9d
Amani Khalid <amanikhalidd@gmail.com>
2025-08-14 08:25:41 +0400

    Role configuration

--- Data/FlightDbContext.cs
+++ Data/FlightDbContext.cs
@@ -469,6 +469,10 @@ namespace FlightManagementCompanyProject.Data
                 .HasMaxLength(50) // Maximum length of 50 characters
                 .HasColumnType("nvarchar(50)"); // Specify the column type as nvarchar(50)
 
+            // Role configuration
+            modelBuilder.Entity<CrewMember>()
+            
+
 
 
 
