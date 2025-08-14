b296841bc96203870f49bb48db128fb8797aaf78
Amani Khalid <amanikhalidd@gmail.com>
2025-08-14 08:28:08 +0400

    Specify the column type as nvarchar(20)

--- Data/FlightDbContext.cs
+++ Data/FlightDbContext.cs
@@ -481,6 +481,7 @@ namespace FlightManagementCompanyProject.Data
                 .Property(cm => cm.LicenseNo) // License number is required
                 .IsRequired()
                 .HasMaxLength(20) // Maximum length of 20 characters
+                .HasColumnType("nvarchar(20)"); // Specify the column type as nvarchar(20)
 
 
 
