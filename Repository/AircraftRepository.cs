70040e831558832793b36b351088b190ceab2c88
Amani Khalid <amanikhalidd@gmail.com>
2025-08-14 11:15:45 +0400

    Method to get aircraft due for maintenance before a specific date

--- Repository/AircraftRepository.cs
+++ Repository/AircraftRepository.cs
@@ -1,5 +1,6 @@
 ﻿using FlightManagementCompanyProject.Data;
 using FlightManagementCompanyProject.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -40,6 +41,16 @@ namespace FlightManagementCompanyProject.Repository
         public Aircraft GetById(int id) => _context.Aircrafts.Find(id); // Get an aircraft record by ID
 
         public IEnumerable<Aircraft> GetAll() => _context.Aircrafts.ToList(); // Get all aircraft records
+
+        public IEnumerable<Aircraft> GetDueForMaintenance(DateTime beforeDate) // Method to get aircraft due for maintenance before a specific date
+        {
+
+            return _context.Aircrafts // Query the Aircrafts DbSet
+                    .Include(a => a.AircraftMaintenances) // Include related AircraftMaintenances
+                    .Where(a => a.AircraftMaintenances.Any(m => m.MaintenanceDate <= beforeDate)) // Filter aircraft that have any maintenance record before the specified date
+                    .ToList(); // Convert the result to a list and return it
+        }
+
 
     }
 }
