62557504b6423f85f67e33b5ad2ee931c5976e15
Amani Khalid <amanikhalidd@gmail.com>
2025-08-17 14:06:08 +0400

    Changed Access Modifier of IPassengerRepository Interface

--- Repository/IPassengerRepository.cs
+++ Repository/IPassengerRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FlightManagementCompanyProject.Repository
 {
-    interface IPassengerRepository
+    public interface IPassengerRepository
     {
     }
 }
