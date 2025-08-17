53593caf64999b5241183fd7d057bab12aae876f
Amani Khalid <amanikhalidd@gmail.com>
2025-08-17 13:57:09 +0400

    Get a flight crew record by ID

--- Repository/IFlightCrewRepository.cs
+++ Repository/IFlightCrewRepository.cs
@@ -12,5 +12,7 @@ namespace FlightManagementCompanyProject.Repository
         void Add(FlightCrew flightCrew); // Add a new flight crew record
         void Delete(FlightCrew flightCrew); // Delete a flight crew record
         IEnumerable<FlightCrew> GetAll(); // Get all flight crew records
+
+        FlightCrew GetById(int id); // Get a flight crew record by ID
     }
 }
