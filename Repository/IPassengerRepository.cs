5b9eaed96cf3ee728c68b7639ed67b15309bb095
Amani Khalid <amanikhalidd@gmail.com>
2025-08-17 14:11:23 +0400

    Add a new passenger record

--- Repository/IPassengerRepository.cs
+++ Repository/IPassengerRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightManagementCompanyProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace FlightManagementCompanyProject.Repository
 {
     public interface IPassengerRepository
     {
+        void Add(Passenger passenger); // Add a new passenger record
+
     }
 }
