77c00960459cb10776308e6c5c955c0f967ffe6d
Amani Khalid <amanikhalidd@gmail.com>
2025-08-17 08:56:18 +0400

    Changed Access Modifier of IAircraftMaintenanceRepository Interface

--- Repository/IAircraftMaintenanceRepository.cs
+++ Repository/IAircraftMaintenanceRepository.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace FlightManagementCompanyProject.Repository
 {
-    interface IAircraftMaintenanceRepository
+    public interface IAircraftMaintenanceRepository
     {
+
     }
 }
