24afe6567fe238feb4630164a6cb67a121270496
Amani Khalid <amanikhalidd@gmail.com>
2025-08-17 08:16:14 +0400

    Add a new baggage record

--- Repository/BaggageRepository.cs
+++ Repository/BaggageRepository.cs
@@ -1,4 +1,5 @@
 ﻿using FlightManagementCompanyProject.Data;
+using FlightManagementCompanyProject.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,12 @@ namespace FlightManagementCompanyProject.Repository
         public BaggageRepository(FlightDbContext context) // Initialize the context
         {
             _context = context;
+        }
+
+        public void Add(Baggage baggage) // Add a new baggage record
+        {
+            _context.Baggages.Add(baggage);
+            _context.SaveChanges();
         }
     }
 }
