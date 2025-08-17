8fc6f224bb1be657c8f54264b31356debf3bca3b
Amani Khalid <amanikhalidd@gmail.com>
2025-08-17 09:01:51 +0400

    Add a new airport record

--- Repository/IAirportRepository.cs
+++ Repository/IAirportRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightManagementCompanyProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace FlightManagementCompanyProject.Repository
 {
     public interface IAirportRepository
     {
+        void Add(Airport airport); // Add a new airport record
     }
 }
