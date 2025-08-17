03a85dd19fa2294b6856ee0848ce57f1c44f608d
Amani Khalid <amanikhalidd@gmail.com>
2025-08-17 13:50:26 +0400

    Changed Access Modifier of ICrewMemberRepository Interface

--- Repository/ICrewMemberRepository.cs
+++ Repository/ICrewMemberRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FlightManagementCompanyProject.Repository
 {
-    interface ICrewMemberRepository
+    public interface ICrewMemberRepository
     {
     }
 }
