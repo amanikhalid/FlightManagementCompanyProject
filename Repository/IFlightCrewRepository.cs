5711e518553361743ad8adc9964644f8ab781589
Amani Khalid <amanikhalidd@gmail.com>
2025-08-17 13:56:28 +0400

    Get all flight crew records

--- Repository/IFlightCrewRepository.cs
+++ Repository/IFlightCrewRepository.cs
@@ -11,5 +11,6 @@ namespace FlightManagementCompanyProject.Repository
     {
         void Add(FlightCrew flightCrew); // Add a new flight crew record
         void Delete(FlightCrew flightCrew); // Delete a flight crew record
+        IEnumerable<FlightCrew> GetAll(); // Get all flight crew records
     }
 }
