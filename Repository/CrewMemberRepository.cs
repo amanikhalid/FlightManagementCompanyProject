2080ddf3dabe6798f6910b80552588a44069c639
Amani Khalid <amanikhalidd@gmail.com>
2025-08-17 08:26:55 +0400

    Interface implementation for crew member repository

--- Repository/CrewMemberRepository.cs
+++ Repository/CrewMemberRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FlightManagementCompanyProject.Repository
 {
-    public class CrewMemberRepository
+    public class CrewMemberRepository : ICrewMemberRepository // Interface implementation for crew member repository
     {
     }
 }
