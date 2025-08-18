39c9382244ce2caea8287ba140160ad0245dae90
Amani Khalid <amanikhalidd@gmail.com>
2025-08-18 08:56:44 +0400

    Constructor to initialize the DbContext

--- Repository/RouteRepository.cs
+++ Repository/RouteRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightManagementCompanyProject.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace FlightManagementCompanyProject.Repository
 {
     public class RouteRepository : IRouteRepository // Interface implementation for RouteRepository
     {
+        private readonly FlightDbContext _context; // Constructor to initialize the DbContext
     }
 }
