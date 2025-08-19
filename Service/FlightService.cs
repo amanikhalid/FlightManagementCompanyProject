f3b31773d039c62a42398f622d232f989996c2e1
Amani Khalid <amanikhalidd@gmail.com>
2025-08-19 14:23:52 +0400

    Models for flight entities

--- Service/FlightService.cs
+++ Service/FlightService.cs
@@ -1,5 +1,6 @@
 ﻿using FlightManagementCompany.Data; // FlightManagementCompany.Data namespace for DbContext
 using FlightManagementCompany.DTO; // Data Transfer Objects for flight information
+using FlightManagementCompany.Models; // Models for flight entities
 using System;
 using System.Collections.Generic;
 using System.Linq;
